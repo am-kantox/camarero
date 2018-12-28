f38eaa9f5f0c5351bf9fe2d4317e85e7d1b7d017
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2018-12-28 07:06:50 +0100

    :v: done

--- test/camarero_test.exs
+++ test/camarero_test.exs
@@ -1,8 +1,47 @@
 defmodule CamareroTest do
   use ExUnit.Case
+  use Plug.Test
+
   doctest Camarero
 
-  test "greets the world" do
-    assert Camarero.hello() == :world
+  @opts Camarero.Handler.init([])
+
+  test "responds with 400 on completely wrong path" do
+    conn = conn(:get, "/foo")
+
+    # Invoke the plug
+    conn = Camarero.Handler.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 400
+    assert conn.resp_body |> Jason.decode!() |> Map.keys() == ~w|error path|
+  end
+
+  test "responds with 404 on missing key" do
+    conn = conn(:get, "/api/v1/heartbeat/foo")
+
+    # Invoke the plug
+    conn = Camarero.Handler.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 404
+    assert conn.resp_body |> Jason.decode!() |> Map.keys() == ~w|error key|
+  end
+
+  test "responds with 200 on existing key" do
+    Camarero.Carta.Heartbeat.put("existing", 42)
+
+    conn = conn(:get, "/api/v1/heartbeat/existing")
+
+    # Invoke the plug
+    conn = Camarero.Handler.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body |> Jason.decode!() |> Map.keys() == ~w|key value|
+    assert conn.resp_body |> Jason.decode!() |> Map.get("value") == 42
   end
 end
